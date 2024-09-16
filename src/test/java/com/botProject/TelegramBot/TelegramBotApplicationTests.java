641eb7bdc4318dfef1849613c231164502bad793
dusha <dushasam26@gmail.com>
2024-09-16 15:43:24 +0200

    reinializing project

---
+++ src/test/java/com/botProject/TelegramBot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botProject.TelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
