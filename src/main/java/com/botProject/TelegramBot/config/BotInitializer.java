f7a59f447c977862221dddf59c3706f269686787
dusha <dushasam26@gmail.com>
2024-09-16 19:41:54 +0200

    added log support

--- src/main/java/com/botProject/TelegramBot/config/BotInitializer.java
+++ src/main/java/com/botProject/TelegramBot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.botProject.TelegramBot.config;
 
 import com.botProject.TelegramBot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,6 +10,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class BotInitializer {
     @Autowired
@@ -21,6 +23,7 @@ public class BotInitializer {
             telegramBotsApi.registerBot(bot);
         }catch (TelegramApiException e){
             //TODO logi
+            log.error("Error occured: " + e.getMessage());
         }
 
     }
